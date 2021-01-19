c17fa34bc3fbc6f93cec4784ebce0d2cdfdab59f
Gustavo Camello <gucamello@gmail.com>
2021-01-19 17:55:48 +0000

    add larger image functionality

--- tests/integration/components/player/image-test.js
+++ tests/integration/components/player/image-test.js
@@ -6,21 +6,15 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | player/image', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Player::Image />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders the given image', async (assert) => {
     await render(hbs`
-      <Player::Image>
-        template block text
-      </Player::Image>
-    `);
+      <Player::Image
+      src="https://images.unsplash.com/photo-1516742720271-6ae39cbc5bd1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+      alt="image of a player"
+      />
+    `)
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.image-container img').exists()
+    .hasAttribute('alt', 'image of a player')
   });
 });
